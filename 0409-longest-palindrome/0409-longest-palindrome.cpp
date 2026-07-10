c4c057f51c07e60381110d43d567373f03b7c231
HarsH GuptA <aharsh0824@gmail.com>
2026-07-10 19:30:05 +0530

    Time: 0 ms (100%), Space: 9.3 MB (68.11%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<int,int> evens;
-        unordered_map<int,int> odds;
         unordered_map<int,int> mpp;
         for(int i=0;i<s.size();i++){
             mpp[(int)s[i]]++;
         }
-        for(auto it:mpp){
-            if(it.second%2==0){
-                evens[it.first] = it.second;
-            }else odds[it.first] = it.second;
-        }
         int ans = 0;
-        for(auto it:evens){
-            ans+=(it.second);
+        for(auto it:mpp){
+            if(it.second%2==0) ans+=(it.second);
+            else ans+=(it.second-1);
         }
-        for(auto it:odds){
-            ans+=(it.second-1);
-        }
-        if(odds.size()!=0) ans++; 
+        if(ans!=s.size()) ans++;
         return ans;
     }
 };
