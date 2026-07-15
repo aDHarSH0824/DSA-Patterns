2094724320504fe4d4eef5f58d85eb49622fcfc4
HarsH GuptA <aharsh0824@gmail.com>
2026-07-15 18:05:56 +0530

    Time: 0 ms (100%), Space: 22.5 MB (77.28%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isitOkay(vector<int>& arr,int mid){
+        int ans = 0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>=mid) ans++;
+        }
+        return ans>=mid;
+    }
+    int hIndex(vector<int>& citations) {
+        int low = 1,high = citations[citations.size()-1];
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(isitOkay(citations,mid)) low=mid+1;
+            else high=mid-1;
+        }
+        return high;
+    }
+};
